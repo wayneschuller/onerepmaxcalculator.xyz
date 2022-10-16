db390c9a14de2c6bc9660c1822d26f2c27a0a9cc
Wayne Schuller <wayne@schuller.id.au>
2022-10-16 18:18:32 +1100

    Add range of e1rm equations

--- src/App.js
+++ src/App.js
@@ -69,12 +69,38 @@ const Result = (props) => {
   );
 }
 
-function estimateE1RM(reps, weight) {
-  console.log(`estimateE1RM: ${reps} ${weight}`);
+// Return a rounded 1 rep max
+// For theory see: https://en.wikipedia.org/wiki/One-repetition_maximum
+function estimateE1RM(reps, weight, equation) {
 
-  if (reps == 1) return weight; // Heavy single requires no estimate!
-  return Math.round(weight / (1.0278 - 0.0278 * reps)); // Brzycki e1rm estimate formula
+  if (reps === 1) return weight; // Heavy single requires no estimate!
+
+  switch (equation) {
+    case "Epley":
+      return Math.round(weight * (1 + reps / 30));
+      break;
+    case "McGlothin":
+      return Math.round((100 * weight) / (101.3 - 2.67123 * reps));
+      break;
+    case "Lombardi":
+      return Math.round(weight * Math.pow(reps, 0.1));
+      break;
+    case "Mayhew":
+      return Math.round((100 * weight) / (52.2 + 41.9 * Math.pow(Math.E, -0.055 * reps)));
+      break;
+    case "OConner":
+      return Math.round(weight * (1 + reps / 40));
+      break;
+    case "Wathen":
+      return Math.round((100 * weight) / (48.8 + 53.8 * Math.pow(Math.E, -0.075 * reps)));
+      break;
+    case "Brzycki":
+      return Math.round(weight / (1.0278 - 0.0278 * reps));
+      break;
+    default: // Repeat Brzycki formula as a default here
+      return Math.round(weight / (1.0278 - 0.0278 * reps));
+      break;
+  }
 }
-
 
 export default App;

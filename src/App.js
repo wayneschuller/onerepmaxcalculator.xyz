101828004b10964adb81fac458844ee14069c06c
Wayne Schuller <wayne@schuller.id.au>
2022-10-19 15:56:54 +1100

    Fix warnings

--- src/App.js
+++ src/App.js
@@ -203,28 +203,20 @@ function estimateE1RM(reps, weight, equation) {
   switch (equation) {
     case "Epley":
       return Math.round(weight * (1 + reps / 30));
-      break;
     case "McGlothin":
       return Math.round((100 * weight) / (101.3 - 2.67123 * reps));
-      break;
     case "Lombardi":
       return Math.round(weight * Math.pow(reps, 0.1));
-      break;
     case "Mayhew":
       return Math.round((100 * weight) / (52.2 + 41.9 * Math.pow(Math.E, -0.055 * reps)));
-      break;
     case "OConner":
       return Math.round(weight * (1 + reps / 40));
-      break;
     case "Wathen":
       return Math.round((100 * weight) / (48.8 + 53.8 * Math.pow(Math.E, -0.075 * reps)));
-      break;
     case "Brzycki":
       return Math.round(weight / (1.0278 - 0.0278 * reps));
-      break;
     default: // Repeat Brzycki formula as a default here
       return Math.round(weight / (1.0278 - 0.0278 * reps));
-      break;
   }
 }
 

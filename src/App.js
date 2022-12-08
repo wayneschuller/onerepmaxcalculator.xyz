7fc445c9ddeee2338a4c64c7cff9adec77fe1775
Wayne Schuller <wayne@schuller.id.au>
2022-12-09 08:49:01 +1100

    Basic react-router-dom setup, not integrated yet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,120 @@
 import './App.css';
 
+
+import * as React from "react";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
 import ResponsiveAppBar from './appBar';
 import OneRepMaxCalculator from './oneRepMaxCalculator';
 
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 
-// Draw the main app page
-function App() {
+// export default function App() {
+//   return (
+//     <div>
+//     <Box sx={{ m: 1 }} md={{ m: 3}} >
+//       <Container maxWidth="lg" sx={{ borderRadius: '6px', border: '1px solid grey', boxShadow: '13', backgroundColor: 'palette.secondary.light' }}>
+//         <OneRepMaxCalculator />
+//       </Container>
+//     </Box>
+//     </div>
+//   );
+// }
+
+export default function App() {
   return (
     <div>
-    <ResponsiveAppBar />
-    <Box sx={{ m: 1 }} md={{ m: 3}} >
-      <Container maxWidth="lg" sx={{ borderRadius: '6px', border: '1px solid grey', boxShadow: '13', backgroundColor: 'palette.secondary.light' }}>
-        <OneRepMaxCalculator />
-      </Container>
-    </Box>
+
+      {/* Routes nest inside one another. Nested route paths build upon
+            parent route paths, and nested route elements render inside
+            parent route elements. See the note about <Outlet> below. */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="calculator" element={<Calculator />} />
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div>
+
+     <ResponsiveAppBar />
+
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/calculator">One Rep Max Calculator</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Calculator() {
+  return (
+    <div>
+     <Box sx={{ m: 1 }} md={{ m: 3}} >
+       <Container maxWidth="lg" sx={{ borderRadius: '6px', border: '1px solid grey', boxShadow: '13', backgroundColor: 'palette.secondary.light' }}>
+         <OneRepMaxCalculator />
+       </Container>
+     </Box>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}

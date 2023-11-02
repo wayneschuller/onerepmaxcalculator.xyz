6fa5fb4ca165de0734b851b03f898afe5d78f1cc
Wayne Schuller <wayne@schuller.id.au>
2023-11-02 17:22:37 +1100

    Add RadixUI slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@
 
 // import Image from "next/image";
 import { Inter } from "next/font/google";
+import * as Slider from "@radix-ui/react-slider";
 // import next from "next"; // Docs say DO NOT do this. https://nextjs.org/docs/messages/import-next
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,7 +23,27 @@ const E1RMCalculator = () => {
       <>
         Estimate your max single based on reps and weight (see{" "}
         <a href="https://en.wikipedia.org/wiki/One-repetition_maximum">Wikipedia article</a> for theory)
+        <Reps />
       </>
     </div>
   );
 };
+
+const Reps = () => (
+  <div>
+    <Slider.Root
+      className="relative flex items-center select-none touch-none  h-5"
+      defaultValue={[50]}
+      max={100}
+      step={1}
+    >
+      <Slider.Track className="bg-black relative grow rounded-full h-[3px]">
+        <Slider.Range className="absolute bg-white rounded-full h-full" />
+      </Slider.Track>
+      <Slider.Thumb
+        className="block w-5 h-5 bg-white shadow-[0_2px_10px] shadow-black rounded-[10px] hover:bg-violet focus:outline-none focus:shadow-[0_0_0_5px] focus:shadow-black "
+        aria-label="Reps"
+      />
+    </Slider.Root>
+  </div>
+);

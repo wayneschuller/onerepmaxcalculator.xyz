95bcfe7af52f8f67e8dcb7536e2c93361a1f6d63
Wayne Schuller <wayne@schuller.id.au>
2023-11-12 18:39:25 +1100

    Refactoring

---
+++ src/components/CalcSlider.js
@@ -0,0 +1,22 @@
+/** @format */
+"use client";
+import * as Slider from "@radix-ui/react-slider";
+
+export const CalcSlider = ({ value, onChange, min, max, step }) => (
+  <Slider.Root
+    className="relative flex items-center select-none touch-none h-5"
+    value={value}
+    min={min}
+    max={max}
+    step={step}
+    onValueChange={onChange}
+  >
+    <Slider.Track className="bg-black relative grow rounded-full h-[3px]">
+      <Slider.Range className="absolute bg-slate-400 rounded-full h-full" />
+    </Slider.Track>
+    <Slider.Thumb
+      className="block w-5 h-5 bg-white shadow-[0_2px_10px] shadow-black rounded-[10px] hover:bg-violet focus:outline-none focus:shadow-[0_0_0_5px] focus:shadow-black "
+      aria-label="Reps"
+    />
+  </Slider.Root>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,10 @@
 
 import { useState, useEffect } from "react";
 import { Inter } from "next/font/google";
-import * as Slider from "@radix-ui/react-slider";
 import * as Switch from "@radix-ui/react-switch";
 import Head from "next/head";
 import { Card } from "../components/Card";
+import { CalcSlider } from "../components/CalcSlider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -220,31 +220,11 @@ const UnitChooser = ({ isMetric, onSwitchChange }) => (
   </div>
 );
 
-// Slider abstraction
-const Slider2 = ({ value, onChange, min, max, step }) => (
-  <Slider.Root
-    className="relative flex items-center select-none touch-none h-5"
-    value={value}
-    min={min}
-    max={max}
-    step={step}
-    onValueChange={onChange}
-  >
-    <Slider.Track className="bg-black relative grow rounded-full h-[3px]">
-      <Slider.Range className="absolute bg-slate-400 rounded-full h-full" />
-    </Slider.Track>
-    <Slider.Thumb
-      className="block w-5 h-5 bg-white shadow-[0_2px_10px] shadow-black rounded-[10px] hover:bg-violet focus:outline-none focus:shadow-[0_0_0_5px] focus:shadow-black "
-      aria-label="Reps"
-    />
-  </Slider.Root>
-);
-
 // Reps input component
 const Reps = ({ reps, onChange }) => {
   return (
     <div>
-      <Slider2 aria-label="Reps" value={reps} max="20" min={1} onChange={onChange} />
+      <CalcSlider aria-label="Reps" value={reps} max="20" min={1} onChange={onChange} />
     </div>
   );
 };
@@ -257,7 +237,7 @@ const Weight = ({ weight, onChange, isMetric }) => {
     max = 250;
   }
 
-  return <Slider2 aria-label="Weight" value={weight} max={max} min={1} onChange={onChange} />;
+  return <CalcSlider aria-label="Weight" value={weight} max={max} min={1} onChange={onChange} />;
 };
 
 // Return a rounded 1 rep max

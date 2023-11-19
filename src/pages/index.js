2549ce91a0ec2ddf7e78a5198d086e8d7a285f67
Wayne Schuller <wayne@schuller.id.au>
2023-11-19 18:14:55 +1100

    Share button icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import { Card } from "../components/Card";
 import { CalcSlider } from "../components/CalcSlider";
 import { estimateE1RM } from "../components/estimateE1RM";
 import { useRouter } from "next/router";
+// import { Button } from "@radix-ui/button";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -203,6 +204,7 @@ const E1RMCalculator = () => {
               {isMetric ? "kg" : "lb"}
             </div>
             <div className="flex flex-row justify-center items-center">(Brzycki formula)</div>
+            <ShareButton />
           </div>
         </Card>
       </div>
@@ -311,3 +313,32 @@ const UnitChooser = ({ isMetric, onSwitchChange }) => (
     </label>
   </div>
 );
+
+const ShareButton = () => {
+  return (
+    <>
+      <button>
+        Share <ShareIcon />
+      </button>
+    </>
+  );
+};
+
+const ShareIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+      />
+    </svg>
+  );
+};

81a3ff1a2f943b63db29cb05aba66be6676f5e71
Wayne Schuller <wayne@schuller.id.au>
2023-11-22 14:07:36 +1100

    Highlight selected nav page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,17 +3,35 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 import { DarkModeToggle } from "@/components/DarkModeToggle";
 
 export default function NavBar() {
-  let blah;
+  const pathname = usePathname();
 
   return (
     <>
       <div className="ml-4 flex flex-row p-2">
         <div className="flex flex-1 flex-row gap-4 ">
-          <Link href="/">Calculator</Link>
-          <Link href="/timer">Timer</Link>
+          <Link
+            href="/"
+            className={cn(
+              "transition-colors hover:text-foreground/80",
+              pathname === "/" ? "text-foreground" : "text-foreground/60",
+            )}
+          >
+            Calculator
+          </Link>
+          <Link
+            href="/timer"
+            className={cn(
+              "transition-colors hover:text-foreground/80",
+              pathname === "/timer" ? "text-foreground" : "text-foreground/60",
+            )}
+          >
+            Timer
+          </Link>
         </div>
         <div className="">
           <DarkModeToggle />
